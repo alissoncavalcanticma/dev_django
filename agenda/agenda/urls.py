1a85a92beb4a0eb45b0ed8938abc5e480d213e90
Alisson C <alissoncavalcanticma@gmail.com>
2021-06-05 00:42:31 -0300

    login, logout e exibição de dados

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -1,7 +1,7 @@
 """agenda URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agenda/', views.lista_eventos),
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
